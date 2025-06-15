76ac31203e43848b032b6fa019176c2f101ebcc4
AndreiReznikov <andrezniksk@yandex.ru>
2025-06-16 01:31:32 +0300

    added background update method

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import styles from "./form.module.css";
+import { applyComplexGradient } from "./Form.utils";
 
 type FormData = {
   field1: string;
@@ -11,19 +12,19 @@ type FormData = {
 
 const allowedCharsRegex = /^[ARNDCEQGHILKMFPSTWYV\-]+$/i;
 
-const LETTER_WIDTH = 13.19;
 const FONT_SIZE = 24;
+const LETTER_WIDTH = 13.19;
 const AMINO_COLORS: Record<string, string> = {
   A: "blue",
-  B: "green",
-  C: "orange",
+  R: "green",
+  N: "orange",
   D: "red",
-  E: "yellow",
-  F: "pink",
-  G: "purple",
-  K: "orange",
-  M: "pink",
-  L: "purple",
+  C: "yellow",
+  E: "pink",
+  Q: "purple",
+  G: "orange",
+  H: "pink",
+  I: "purple",
 };
 
 interface SequenceProps {
@@ -70,29 +71,61 @@ export function Form() {
     useState<boolean>(false);
   const sequenceElementsRef = useRef<(HTMLDivElement | null)[]>([]);
 
-  useEffect(() => {
-    if (!isAllSequencesMounted) return;
-
+  const updateSequencesBackground = useCallback(() => {
     const sequencesCount = sequenceElementsRef.current?.length;
     const lineHeight = FONT_SIZE * sequencesCount;
-    const referenceSequence = sequenceElementsRef.current[0];
     const referenceSequenceAminoChain = sequences[0]?.split("");
     const referenceSequenceColors = referenceSequenceAminoChain.map(
       (amino) => AMINO_COLORS[amino] ?? "transparent"
     );
 
-    console.log(
-      sequencesCount,
-      lineHeight,
-      referenceSequence,
-      referenceSequenceAminoChain,
-      referenceSequenceColors
-    );
+    sequenceElementsRef.current.forEach((sequenceElement, index) => {
+      sequenceElement?.style.setProperty("font-size", `${FONT_SIZE}px`);
+      sequenceElement?.style.setProperty("line-height", `${lineHeight}px`);
+      sequenceElement?.style.setProperty("top", `${index * FONT_SIZE}px`);
 
-    sequenceElementsRef.current.forEach((sequenceElement) => {
-      sequenceElement?.style.setProperty("background", "aqua");
+      const sequence = sequences[index].split("");
+      const sequenceWidth = sequenceElement?.clientWidth ?? 0;
+      const sequenceLettersRatio = sequenceWidth / LETTER_WIDTH;
+      const rowLettersNumber =
+        sequenceLettersRatio - Math.floor(sequenceLettersRatio) > 0.95
+          ? Math.ceil(sequenceLettersRatio)
+          : Math.floor(sequenceLettersRatio);
+      const rowsNumber = Math.ceil(sequence.length / rowLettersNumber);
+      console.log(LETTER_WIDTH * sequence.length, sequenceWidth);
+      const sequenceColors =
+        index === 0
+          ? referenceSequenceColors
+          : sequence?.map((amino, index) =>
+              amino === referenceSequenceAminoChain[index]
+                ? "transparent"
+                : AMINO_COLORS[amino]
+            );
+
+      applyComplexGradient(sequenceElement, {
+        colorStep: LETTER_WIDTH,
+        gradientCount: rowsNumber,
+        colorsPerGradient: rowLettersNumber,
+        rowHeight: FONT_SIZE,
+        colorSets: sequenceColors,
+        initialY: (lineHeight - FONT_SIZE) / 2,
+        yStep: lineHeight,
+      });
     });
-  }, [isAllSequencesMounted, sequences]);
+  }, [sequences]);
+
+  useEffect(() => {
+    if (!isAllSequencesMounted) return;
+
+    updateSequencesBackground();
+
+    const handleResize = () => updateSequencesBackground();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isAllSequencesMounted, sequences, updateSequencesBackground]);
 
   const onSubmit = useCallback(
     (data: FormData) => {
