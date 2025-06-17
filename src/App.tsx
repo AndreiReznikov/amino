78ba412532a7e073bf1eba7c504120e2692e745c
AndreiReznikov <andrezniksk@yandex.ru>
2025-06-18 00:41:03 +0300

    added CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
 import { SequencesPage } from "./pages/SequencesPage";
 import styles from "./app.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
+      <CssBaseline />
       <SequencesPage />
     </div>
   );
