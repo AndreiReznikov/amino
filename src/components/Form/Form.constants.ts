47517747614d3babb250c9e61410617d6bb63367
AndreiReznikov <andrezniksk@yandex.ru>
2025-06-17 19:01:10 +0300

    added huge font

--- src/components/Form/Form.constants.ts
+++ src/components/Form/Form.constants.ts
@@ -4,8 +4,9 @@ export const ALLOWED_CHARS_REGEX = /^[ARNDCEQGHILKMFPSTWYV\-]+$/i;
 export const SEQUENCE_FONT_OPTIONS = {
   small: { fontSize: 18, letterWidth: 9.9, label: "Масштаб - 1x" },
   medium: { fontSize: 36, letterWidth: 19.8, label: "Масштаб - 2x" },
-  xlarge: { fontSize: 54, letterWidth: 29.7, label: "Масштаб - 3x" },
-  large: { fontSize: 72, letterWidth: 39.5, label: "Масштаб - 4x" },
+  large: { fontSize: 54, letterWidth: 29.7, label: "Масштаб - 3x" },
+  xlarge: { fontSize: 72, letterWidth: 39.5, label: "Масштаб - 4x" },
+  huge: { fontSize: 90, letterWidth: 49.5, label: "Масштаб - 5x" },
 } as const;
 
 export const aminoAcidGroups: Record<AminoAcid, AminoAcidGroup> = {
